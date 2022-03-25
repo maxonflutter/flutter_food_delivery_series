2e3149072d592a1cb28d02a0653a4cbcbf803fba
Massimo Del Pezzo <massimo.dpezzo@gmail.com>
2022-03-25 11:11:50 +0100

    Vouchers bloc in the barrel file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_food_delivery_app/blocs/voucher/voucher_bloc.dart';
-import 'package:flutter_food_delivery_app/repositories/voucher/voucher_repository.dart';
 
 import 'blocs/blocs.dart';
 import 'repositories/repositories.dart';
