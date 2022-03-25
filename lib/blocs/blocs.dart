443b5d737cae6f100c6d13bc9aeb5981e17e2a8b
Massimo Del Pezzo <massimo.dpezzo@gmail.com>
2022-03-25 11:12:04 +0100

    Voucher bloc in the barrel file

--- lib/blocs/blocs.dart
+++ lib/blocs/blocs.dart
@@ -3,3 +3,4 @@ export 'place/place_bloc.dart';
 export 'autocomplete/autocomplete_bloc.dart';
 export 'filter/filter_bloc.dart';
 export 'basket/basket_bloc.dart';
+export 'voucher/voucher_bloc.dart';
